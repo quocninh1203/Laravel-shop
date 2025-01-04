719dfb3e8581eda9c86b00f7e8f90a1c898dce4d
unknown <hd16032000@gmail.com>
2025-01-04 22:34:24 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
+
+
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
