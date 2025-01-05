c477a5d508404071602d50e0c0702cf017044c55
unknown <hd16032000@gmail.com>
2025-01-06 01:36:33 +0700

    update project

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Brand;
+
+class AdminController extends Controller
+{
+    public function index(){
+        return view('admin.index');
+    }
+
+    public function brands(){
+        $brands = Brand::orderBy('id','DESC')->paginate(10);
+        return view('admin.brands',compact('brands'));
+    }
+}
--- database/migrations/2025_01_04_171715_create_brands_table.php
+++ database/migrations/2025_01_04_171715_create_brands_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('brands', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('mobile')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('utype')->default('USR')->comment('ADM for Admin and USR for User or Customer');
-            $table->rememberToken();
+            $table->string('slug')->unique();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
@@ -29,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('brands');
     }
 };
