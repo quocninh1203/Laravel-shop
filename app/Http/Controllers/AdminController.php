b98e5d2ae3e864369a2a92952f3e90a8fcc57798
quocninh <quocninh9.32017@gmail.com>
2025-01-09 00:36:55 +0700

    Admin Category Page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Support\Str;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\File;
@@ -94,4 +95,11 @@ class AdminController extends Controller
         $brand->delete();
         return redirect()->route('admin.brands')->with('status','Brand has been deleted successfully');
     }
+
+
+    public function categories()
+ {
+        $categories = Category::orderBy('id','DESC')->paginate(10);
+        return view("admin.categories",compact('categories'));
+ }
 }
