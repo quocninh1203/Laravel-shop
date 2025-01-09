1523ffce66b83c1f8bec27ec152f1bb916a7527b
unknown <hd16032000@gmail.com>
2025-01-10 05:12:22 +0700

    add product

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,16 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CartController;
 
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
 Route::get('/shop',[ShopController::class,'index'])->name('shop.index');
+Route::get('/shop/{product_slug}',[ShopController::class,'product_details'])->name("shop.product.details");
+
+Route::get('/cart',[CartController::class,'index'])->name('cart.index');
+Route::post('/cart/add', [CartController::class, 'add_to_cart'])->name('cart.add');
 
 
 Route::middleware(['auth'])->group(function(){
